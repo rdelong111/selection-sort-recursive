5c2f1fc8c42e0d71621419dd6eb5cb9aeb3d8012
RyanDeLong <rdelong111@gmail.com>
2022-03-09 10:37:10 -0600

    commit tree d286e8858e7ed9bca9a9e96ac441eb94c5332d31

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,5 +1,19 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  
+  if (typeof arr[0] === 'object') {
+    return arr[0].list;
+  }
+  else {
+    if (typeof arr[arr.length - 1] === 'object') {
+      const min = Math.min(...arr.slice(0, arr.length - 1));
+      const idx = arr.indexOf(min);
+      arr[arr.length - 1].list.push(arr.splice(idx, 1)[0]);
+    }
+    else {
+      arr.push({'list': []});
+    }
+    return selectionSortRecursive(arr);
+  }
 }
 
 if (require.main === module) {
@@ -12,5 +26,23 @@ if (require.main === module) {
 
 module.exports = selectionSortRecursive;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/*
+If arr has been iterated through
+  return the sorted list
+If above fails
+  If there is a place to store the sorted list at the end of arr
+    Store the value of the min of arr
+    Store the value of the index of min of arr
+    Push the min into the sorted array
+  If above fails
+    Generate a spot at end of arr to store the list of sorted values
+  Return the function selectionSortRecursive with arr as the argument
+*/
+
+/*
+The first IF statement tests if the array has been iterated through by seeing if the first element of the array is an object that has the sorted list stored inside.
+If the array has not been iterated through/the object is not first, then another IF statement is encountered.
+This IF statement test if this is the first iteration of the array. If it is, than an object with a spot to store the sorted array is pushed to the end of arr.
+Otherwise, The min is found in the array. The min is pushed in the stored object array. The min is then removed from arr.
+Once the inside IF statement is completed, then the function is returned with arr.
+*/
